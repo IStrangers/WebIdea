23c951b1c33e46d301442bf24be91a81499fcffd
qqq000qqq@aliyun.com <xqq1258460261>
2022-06-01 18:33:53 +0800

    20220601

---
+++ src/components/CodeEditor.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="Code-Editor">
+    <div class="Code-Line-Number-Container">
+      <div class="Code-Line-Number">
+
+      </div>
+    </div>
+    <div class="Code-Line-Container">
+      <div class="Code-Line">
+
+      </div>
+    </div>
+    <textarea class="Code-Input"></textarea>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+  },
+})
+export default class CodeEditor extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.Code-Editor {
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  cursor: text;
+
+  .Code-Line-Number-Container{
+    height: 100%;
+    width: 50px;
+    min-width: 50px;
+    border: 1px solid;
+
+    .Code-Line-Number{
+
+    }
+  }
+
+
+  .Code-Input::after {
+    content:'';
+    display: block;
+    width:1px;
+    height:16px;
+    animation: blink 1s infinite steps(1, start)
+  }
+
+  @keyframes blink {
+    0%{
+      background-color: white;
+    }
+    50% {
+      background-color: black;
+    }
+    100% {
+      background-color: white;
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <CodeEditor></CodeEditor>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import CodeEditor from '@/components/CodeEditor.vue'
 
 @Options({
   components: {
-    HelloWorld,
+    CodeEditor
   },
 })
 export default class HomeView extends Vue {}
